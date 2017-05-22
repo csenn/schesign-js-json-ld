7ad4cbc3aa03927cdcb0123d78bb2db287a7550b
csenn <csennabaum@gmail.com>
2017-05-22 11:08:19 +0800

    update json ld to new graph

--- src/constants.js
+++ src/constants.js
@@ -1,64 +1,47 @@
 
-const prefix = 'https://www.schesign.com/datatype/';
+export const PREFIX = 'https://www.schesign.com/datatype/'
 
-export const TYPE_BOOLEAN = `${prefix}boolean`;
-export const TYPE_TEXT = `${prefix}text`;
-export const TYPE_URL = `${prefix}url`;
-export const TYPE_EMAIL = `${prefix}email`;
-export const TYPE_ENUM = `${prefix}enum`;
-export const TYPE_HOSTNAME = `${prefix}hostname`;
-export const TYPE_NUMBER = `${prefix}number`;
-export const TYPE_INT = `${prefix}int`;
-export const TYPE_INT_8 = `${prefix}int8`;
-export const TYPE_INT_16 = `${prefix}int16`;
-export const TYPE_INT_32 = `${prefix}int32`;
-export const TYPE_INT_64 = `${prefix}int64`;
-export const TYPE_FLOAT_32 = `${prefix}float32`;
-export const TYPE_FLOAT_64 = `${prefix}float64`;
-export const TYPE_DATETIME = `${prefix}datetime`;
-export const TYPE_SHORT_DATE = `${prefix}shortdate`;
-export const TYPE_TIME = `${prefix}time`;
+export const TYPE_BOOLEAN = `${PREFIX}boolean`
+export const TYPE_TEXT = `${PREFIX}text`
+export const TYPE_URL = `${PREFIX}url`
+export const TYPE_EMAIL = `${PREFIX}email`
+export const TYPE_ENUM = `${PREFIX}enum`
+export const TYPE_HOSTNAME = `${PREFIX}hostname`
+export const TYPE_NUMBER = `${PREFIX}number`
+export const TYPE_INT = `${PREFIX}int`
+export const TYPE_INT_8 = `${PREFIX}int8`
+export const TYPE_INT_16 = `${PREFIX}int16`
+export const TYPE_INT_32 = `${PREFIX}int32`
+export const TYPE_INT_64 = `${PREFIX}int64`
+export const TYPE_FLOAT_32 = `${PREFIX}float32`
+export const TYPE_FLOAT_64 = `${PREFIX}float64`
+export const TYPE_DATETIME = `${PREFIX}datetime`
+export const TYPE_SHORT_DATE = `${PREFIX}shortdate`
+export const TYPE_TIME = `${PREFIX}time`
 
 /* Speical types */
-export const LINKED_CLASS = 'LinkedClass';
-export const NESTED_OBJECT = 'NestedObject';
+export const LINKED_CLASS = 'LinkedClass'
+export const NESTED_OBJECT = 'NestedObject'
 
 /* Main Primitives */
-export const TEXT = 'Text';
-export const NUMBER = 'Number';
-export const BOOLEAN = 'Boolean';
-export const DATE = 'Date';
-export const ENUM = 'Enum';
+export const TEXT = 'Text'
+export const NUMBER = 'Number'
+export const BOOLEAN = 'Boolean'
+export const DATE = 'Date'
+export const ENUM = 'Enum'
 
-export const TEXT_FORMAT_URL = 'Url';
-export const TEXT_FORMAT_EMAIL = 'Email';
-export const TEXT_FORMAT_HOSTNAME = 'Hostname';
+export const TEXT_FORMAT_URL = 'Url'
+export const TEXT_FORMAT_EMAIL = 'Email'
+export const TEXT_FORMAT_HOSTNAME = 'Hostname'
 
-export const DATE_SHORT = 'ShortDate';
-export const DATE_DATETIME = 'DateTime';
-export const DATE_TIME = 'Time';
+export const DATE_SHORT = 'ShortDate'
+export const DATE_DATETIME = 'DateTime'
+export const DATE_TIME = 'Time'
 
-export const NUMBER_INT = 'Int';
-export const NUMBER_INT_8 = 'Int8';
-export const NUMBER_INT_16 = 'Int16';
-export const NUMBER_INT_32 = 'Int32';
-export const NUMBER_INT_64 = 'Int64';
-export const NUMBER_FLOAT_32 = 'Float32';
-export const NUMBER_FLOAT_64 = 'Float64';
-
-export function reduceUrl(url) {
-  return url.substring(url.indexOf('schesign.com'));
-}
-
-
-export function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-export function isRequiredCardinality(cardinality) {
-  return cardinality.minItems > 0;
-}
-
-export function isMultipleCardinality(cardinality) {
-  return !isNumber(cardinality.maxItems) || cardinality.maxItems > 1;
-}
+export const NUMBER_INT = 'Int'
+export const NUMBER_INT_8 = 'Int8'
+export const NUMBER_INT_16 = 'Int16'
+export const NUMBER_INT_32 = 'Int32'
+export const NUMBER_INT_64 = 'Int64'
+export const NUMBER_FLOAT_32 = 'Float32'
+export const NUMBER_FLOAT_64 = 'Float64'
--- tests/index.js
+++ tests/index.js
@@ -1,77 +1,43 @@
-import { expect } from 'chai';
-import { generateFromGraph } from '../src';
-const { describe, it } = global;
+import { expect } from 'chai'
+import { generateFromGraph } from '../src'
 
-// import simple from './fixtures/simple.json';
-// import simpleSchema from './fixtures/simpleSchema.json';
-// import propertyVariationsSchema from './fixtures/propertyVariationsSchema.json';
+import basic from 'schesign-graph-examples/graphs/export/basic'
+import propertyVariations from 'schesign-graph-examples/graphs/export/property_variations'
+import inheritanceChain from 'schesign-graph-examples/graphs/export/inheritance_chain_2'
+import recursion from 'schesign-graph-examples/graphs/export/recursion'
+import linkedNodes from 'schesign-graph-examples/graphs/export/linked_nodes_2'
 
-// import inheritanceSchema from './fixtures/inheritanceSchema.json';
-// import inheritanceOverride from './fixtures/inheritanceOverride.json';
-// import inheritanceOverrideSchema from './fixtures/inheritanceOverrideSchema.json';
-// import excludeParent from './fixtures/excludeParent';
-// import excludeParentSchema from './fixtures/excludeParentSchema';
-// import recursive from './fixtures/recursive';
-// import recursiveSchema from './fixtures/recursiveSchema';
-// import schemaDotOrg from './fixtures/schemaDotOrg';
+import basicLd from './fixtures/basic.ld.json'
+import propertyVariationsLd from './fixtures/property_variations.ld.json'
+import recursionLd from './fixtures/recursion.ld.json'
+import inheritanceLd from './fixtures/inheritance.ld.json'
+import linkedNodesLd from './fixtures/linked_nodes.ld.json'
 
-import propertyVariations from './fixtures/propertyVariations.json';
-import propertyVariationsLd from './fixtures/propertyVariations.ld.json';
-import inheritance from './fixtures/inheritance.json';
-import inheritanceLd from './fixtures/inheritance.ld.json';
+const { describe, it } = global
 
 describe('generateFromGraph', () => {
-  // it('should convert simple to a json schema', () => {
-  //   const schema = generateFromClass(
-  //     simple.graph,
-  //     'https://www.schesign.com/u/my_user/my_design/0.0.1/class/class1'
-  //   );
-  //   expect(schema).to.deep.equal(simpleSchema);
-  // });
+  it('should convert basic to a json schema', () => {
+    const schema = generateFromGraph(basic.graph)
+    expect(schema).to.deep.equal(basicLd)
+  })
+
+  it('should convert recursion to a json schema', () => {
+    const result = generateFromGraph(recursion.graph)
+    expect(result).to.deep.equal(recursionLd)
+  })
 
   it('should convert propertyVariations to json ld', () => {
-    const result = generateFromGraph(propertyVariations.graph);
-    // console.log(JSON.stringify(result, null, 2));
-    expect(result).to.deep.equal(propertyVariationsLd);
-  });
+    const schema = generateFromGraph(propertyVariations.graph)
+    expect(schema).to.deep.equal(propertyVariationsLd)
+  })
 
   it('should convert inheritance to json ld', () => {
-    const result = generateFromGraph(inheritance.graph);
-    console.log(JSON.stringify(result, null, 2));
-    expect(result).to.deep.equal(inheritanceLd);
-  });
+    const result = generateFromGraph(inheritanceChain.graph)
+    expect(result).to.deep.equal(inheritanceLd)
+  })
 
-  // it('should convert excludeParent to a json schema', () => {
-  //   const schema = generateFromClass(
-  //     excludeParent.graph,
-  //     'https://www.schesign.com/u/my_user/my_design/0.0.1/class/class2'
-  //   );
-  //   expect(schema).to.deep.equal(excludeParentSchema);
-  // });
-
-  // it('should convert inheritanceOverride to a json schema', () => {
-  //   const schema = generateFromClass(
-  //     inheritanceOverride.graph,
-  //     'https://www.schesign.com/u/csenn/test_inheritance_override_1/master/class/class1'
-  //   );
-  //   expect(schema).to.deep.equal(inheritanceOverrideSchema);
-  // });
-
-  // it('should convert recursive to a json schema', () => {
-  //   const schema = generateFromClass(
-  //     recursive.graph,
-  //     'https://www.schesign.com/o/tests/recursive/master/class/class1'
-  //   );
-  //   expect(schema).to.deep.equal(recursiveSchema);
-  //   // expect(schema).to.deep.equal(inheritanceOverrideSchema);
-  // });
-
-  // it.skip('should convert schemaDotOrg to a json schema', () => {
-  //   const schema = generateFromClass(
-  //     schemaDotOrg.graph,
-  //     'https://www.schesign.com/u/csenn/schemadotorg/master/class/person'
-  //   );
-  //   console.log(JSON.stringify(schema, null, 2));
-  //   // expect(schema).to.deep.equal(inheritanceOverrideSchema);
-  // });
-});
+  it('should convert linkedNodes to json ld', () => {
+    const result = generateFromGraph(linkedNodes.graph)
+    expect(result).to.deep.equal(linkedNodesLd)
+  })
+})
